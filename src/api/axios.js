0ac9529535c1d7c49c8ff4c87df48d1ef9f5373e
Betacraft <hoangkimc9@gmail.com>
2024-04-14 13:01:37 +0700

    Ready to deploy #2

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
-import axios from 'axios'
+import axios from "axios";
 
 export default axios.create({
-  baseURL: 'http://localhost:8904',
-})
+  baseURL: "https://to-do-list-api-n5zr.onrender.com",
+});
